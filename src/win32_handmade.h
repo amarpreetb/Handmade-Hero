9fbf41f97c7c05871bd0c816b6c46442b962e54e
Amar B <amarpreet.bhachu@gmail.com>
2017-11-03 15:24:06 -0400

    Day 19

--- src/win32_handmade.h
+++ src/win32_handmade.h
@@ -1,29 +1,37 @@
 #if !defined(WIN32_HANDMADE_H)
 
-struct win32OffScreenBuffer
+struct win32_off_screen_buffer
 {
 	BITMAPINFO Info;
 	void *Memory;
 	int Width;
 	int Height;
 	int Pitch;
-	int BytesPerPixel = 4;
+	int BytesPerPixel;
 };
 
-struct win32WindowDimension
+struct win32_window_dimension
 {
 	int Width;
 	int Height;
 };
 
-struct win32SoundOutput
+struct win32_sound_output
 {
 	int SamplesPerSecond;
 	uint32 RunningSampleIndex;
 	int BytesPerSample;
-	int SecondaryBufferSize;
+	//int SecondaryBufferSize;
+	DWORD SecondaryBufferSize;
 	real32 tSine;
 	int LatencySampleCount;
+};
+
+struct win32_debug_time_marker
+{
+	DWORD PlayCursor;
+	DWORD WriteCursor;
+
 };
 
 #define  WIN32_HANDMADE_H
