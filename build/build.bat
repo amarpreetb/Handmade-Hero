c95721a3f4f75367b24108fe191007ade716a42a
Amar B <amarpreet.bhachu@gmail.com>
2017-11-15 22:17:17 -0500

    Update (Orignal Build)

--- build/build.bat
+++ build/build.bat
@@ -1,8 +1,13 @@
-cl -GR -EHa -Oi -wd4201 -wd4100 -wd4189 -wd4800 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE-WIN32=1 -FC -Zi win32_handmade.cpp user32.lib gdi32.lib winmm.lib
+@echo off
+
+set CommonCompilerFlags=-MT -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4456 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7
+set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib
+
 
 REM 32-bit build
-REM cl %CommonCompilerFlags% win32_handmade.cpp /link %CommonCompilerFlags%
+cl %CommonCompilerFlags% ..\handmade\code\win32_handmade.cpp /link -subsystem:windows,5.1 %CommonLinkerFlags%
 
 REM 64-bit build
-cl %CommonCompilerFlags% handmade.cpp -Fmhanmade.map /link /DLL /EXPORT:GameGetSoundSamples /EXPORT:GameUpdateAndRender
-cl %CommonCompilerFlags% win32_handmade.cpp -Fmwin32_hanmade.map /link %CommonCompilerFlags%
+cl %CommonCompilerFlags% handmade.cpp -Fmwin32_handmade.map /LD /link /EXPORT:GameGetSoundSamples /EXPORT:GameUpdateAndRender
+cl %CommonCompilerFlags% win32_handmade.cpp -Fmwin32_handmade.map /link %CommonLinkerFlags%
+popd
